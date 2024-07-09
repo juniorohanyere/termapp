8baa6d02649f61076f95c19443ece9b0585a01c5
Junior Ohanyere <junohanyere@gmail.com>
2024-07-09 20:35:11 +0100

    _tgui module clean from bugs and coding style errors

--- tgui/_tgui.py
+++ tgui/_tgui.py
@@ -2,28 +2,28 @@
 """
 
 import curses
-from curses import wrapper
 
 
 class TGUI:
     """Base class for Terminal GUI.
     """
 
-    def __init__(self, a_dict={}):
+    def __init__(self):
         """Initialize self. See help(type(self)) for accurate signature.
         """
 
-        self._kwargs = a_dict
         self._unblock = False
+        self._stdscr = None
+
+        self._color_pairs = {}
 
     def __app__(self, arg):
-        """Callback method for a wrapper function (currently curses') called by
+        """Callback method for a wrapper function (curses') called by
         the run method.
-        Serves as the entry point for a user program.
+        Serves as the entry point for a user program for Terminal GUI.
 
         Args:
-            arg: first and only parameter to be used by a wrapper
-            function.
+            arg: first and only parameter to be used by the wrapper function.
         """
 
         self._stdscr = arg
@@ -31,7 +31,25 @@ class TGUI:
         # set screen properties
         curses.curs_set(0)
 
+        # initilize color pairs
+        self._color_pairs = self._set_color_pairs()
+
         # other curses inits goes here...
+
+    def _set_color_pairs(self):
+        pair_no = 0
+        colors = {}
+
+        for i in range(curses.COLORS):
+            for j in range(curses.COLORS):
+                pair_no += 1
+                curses.init_pair(pair_no, i, j)
+                color_no = curses.color_pair(pair_no)
+
+                color = (i, j)  # color = (fg, bg)
+                colors.update([(color, color_no)])
+
+        return colors
 
     def run(self):
         """Run the terminal GUI application.
@@ -39,4 +57,9 @@ class TGUI:
 
         curses.wrapper(self.__app__)
         if not self._unblock:
+            curses.cbreak()
+
             self._stdscr.getch()
+
+            curses.nocbreak()
+            curses.endwin()
